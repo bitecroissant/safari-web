9c35f68cec456506e39a33d3d9de5681feda455c
bitecroissant <bitecroissant@hotmail.com>
2024-11-13 12:42:38 +0800

    引入 axios 测试跨域请求能否成功

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,24 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export const Home: React.FC = () => {
+    const [x, setX] = useState('')
+    useEffect(() => {
+        axios.create({
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            timeout: 5000,
+        })
+        axios.get('https://api.muggle.cc/solarTerms/next')
+        .then((xxx) => {
+            setX(JSON.stringify(xxx))
+        })
+    }, [])
     return (
-        <div>home</div>
+        <div>
+            <header>home</header>
+            <p>{x}</p>
+        </div>
     )
 }
