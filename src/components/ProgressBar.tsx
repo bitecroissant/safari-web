ab5d3e336972a85098860ebe8d67ca536d7f0f39
bitecroissant <bitecroissant@hotmail.com>
2024-11-26 21:32:54 +0800

    更新进度条上面的年份自动获取

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import s from './ProgressBar.module.scss'
+import { time } from '../lib/time'
 
 interface Props {
   percentage: number
@@ -17,7 +18,7 @@ export const ProgressBar: React.FC<Props> = ({ percentage = 0 }) => {
       <div className={s.progressBar}>
         <div className={s.actualBar} style={{ width: `${innerPercentage}%` }}></div>
         <div className={s.dot} style={{ left: innerPercentage === 0 ? 0 : `calc(${innerPercentage}% - 14px)` }}></div>
-        <h5 absolute top='0' style={{ left: innerPercentage > 50 ? '10px' : 'calc(100% - 60px)' }} text="[var(--color-tertiary)]">2024 年</h5>
+        <h5 absolute top='0' style={{ left: innerPercentage > 50 ? '10px' : 'calc(100% - 60px)' }} text="[var(--color-tertiary)]">{ time().year }</h5>
       </div>
   )
 }
